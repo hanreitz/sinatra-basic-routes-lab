bb7476f0700c93feb543e973fe5b93162f1117ef
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-02-11 17:11:49 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Hannah"
+  end
+
+  get '/hometown' do
+    "My hometown is Charlotte, NC"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is I have no idea"
+  end
+
 end
